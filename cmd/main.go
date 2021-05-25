e5c3a3d8b290aaa3d0764d3edd6f85e9be7249d1
Firdavs <firdavs.2001.fed@gmail.com>
2021-05-25 09:04:23 +0500

    add new func

--- cmd/main.go
+++ cmd/main.go
@@ -5,30 +5,24 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"fmt"
+	// "fmt"
 )
 
 func main() {
 	host := "0.0.0.0"
-	port := "3939"
-
+	port := "8080"
 	if err := execute(host, port); err != nil {
 		os.Exit(1)
 	}
 }
-
 func execute(host string, port string) (err error) {
 	mux := http.NewServeMux()
 	bannersSvc := banners.NewService()
 	server := app.NewServer(mux, bannersSvc)
-	server.Init()
-
 	srv := &http.Server{
 		Addr:    net.JoinHostPort(host, port),
 		Handler: server,
 	}
-
-	fmt.Println("Server is listening...")
-
+	server.Init()
 	return srv.ListenAndServe()
 }
--- pkg/banners/server.go
+++ pkg/banners/server.go
@@ -6,61 +6,30 @@ import (
 	"sync"
 )
 
-// Service ...
 type Service struct {
 	mu    sync.RWMutex
 	items []*Banner
 }
 
-// NewService ...
 func NewService() *Service {
 	return &Service{items: make([]*Banner, 0)}
 }
 
-func (s *Service) Init() *Service {
-	s.items = append(s.items,
-		&Banner{
-			ID:      1,
-			Title:   "title1",
-			Content: "content1",
-			Button:  "button1",
-			Link:    "link1",
-		},
-		&Banner{
-			ID:      2,
-			Title:   "title2",
-			Content: "content2",
-			Button:  "button2",
-			Link:    "link2",
-		},
-		&Banner{
-			ID:      3,
-			Title:   "title3",
-			Content: "content3",
-			Button:  "button3",
-			Link:    "link3",
-		},
-	)
-	return s
-}
-
-// Banner ...
 type Banner struct {
 	ID      int64
 	Title   string
 	Content string
 	Button  string
 	Link    string
+	Image   string
 }
 
-// All ...
 func (s *Service) All(ctx context.Context) ([]*Banner, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	return s.items, nil
 }
 
-// ByID ...
 func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
@@ -69,40 +38,44 @@ func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
 			return banner, nil
 		}
 	}
-
 	return nil, errors.New("item not found")
 }
 
-// Save ...
+var curID int64 = 0
+
 func (s *Service) Save(ctx context.Context, item *Banner) (*Banner, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	id := len(s.items) + 1
-
 	if item.ID == 0 {
-		item.ID = int64(id)
+		curID++
+		item.ID = curID
 		s.items = append(s.items, item)
-	} else {
-		for i, banner := range s.items {
-			if banner.ID == item.ID {
-				s.items[i] = item
+		return item, nil
+	}
+	for i := 0; i < len(s.items); i++ {
+		if s.items[i].ID == item.ID {
+			if item.Image == "" {
+				item.Image = s.items[i].Image
 			}
+			s.items[i] = item
+			return item, nil
 		}
 	}
-	return item, nil
+	return nil, errors.New("item not found")
 }
 
-// RemoveByID ...
 func (s *Service) RemoveByID(ctx context.Context, id int64) (*Banner, error) {
+	banner, err := s.ByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	index := -1
 	s.mu.RLock()
-	defer s.mu.RUnlock()
-	for i, banner := range s.items {
-		if banner.ID == id {
-			s.items = append(s.items[:i], s.items[i+1:]...)
-			return banner, nil
+	for i := 0; i < len(s.items); i++ {
+		if s.items[i] == banner {
+			index = i
+			break
 		}
 	}
-
-	return nil, errors.New("item not found")
+	s.items = append(s.items[:index], s.items[index+1:]...)
+	s.mu.RUnlock()
+	return banner, nil
 }
